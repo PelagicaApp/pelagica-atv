c25f2a5251dec40b708de6e0cf757e90871547cc
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-09-06 14:28:51 +0200

    Initial Commit

---
+++ Pelagica/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pelagica
+//
+//  Created by Jan Straßburger on 06.09.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pelagica/PelagicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PelagicaApp.swift
+//  Pelagica
+//
+//  Created by Jan Straßburger on 06.09.26.
+//
+
+import SwiftUI
+
+@main
+struct PelagicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
